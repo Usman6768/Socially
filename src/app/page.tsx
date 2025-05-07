43d9c67a018e54b15e6bfb1780354816f98a9c13
Usman Pervez <usmanpervez5@gmail.com>
2025-05-07 12:43:06 +0500

    Setup and auth with clerk

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button className="bg-red-500">
+            Sign In
+          </button>
+        </SignInButton>
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+}
