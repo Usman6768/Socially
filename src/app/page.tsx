c1b5d4d6a99547f9106ea2ecd5e5cca968e5802d
Usman Pervez <usmanpervez5@gmail.com>
2025-05-08 21:40:33 +0500

    Implement user synchronization and update Prisma schema for relationships

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>Home Page content</h1>
